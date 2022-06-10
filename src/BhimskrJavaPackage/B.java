08b6ad3fa0b3479c0e320768619996c1e3b27e48
HP <HP@Ramya>
2022-06-10 23:01:50 +0530

    This is my second commit

---
+++ src/BhimskrJavaPackage/B.java
@@ -0,0 +1,16 @@
+package BhimskrJavaPackage;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20;
+		int b=10;
+		int c=a-b;
+		System.out.println(c);
+		
+		
+	}
+
+}
